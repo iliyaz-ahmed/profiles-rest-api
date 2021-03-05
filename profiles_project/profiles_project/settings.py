7a014c545c15d996deff556c64f4b94b2c9b3fc5
Iliyas <ahmed.iliyaz96@gmail.com>
2021-03-05 10:13:34 +0530

    allowed hosts

--- profiles_project/profiles_project/settings.py
+++ profiles_project/profiles_project/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%i@m8i4&)^!ldvsy79s1=d^-a^cx=xat)2udbv11gt(iru^m&!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-3-141-85-78.us-east-2.compute.amazonaws.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -123,3 +126,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
